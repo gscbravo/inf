9530e4ed93d62db8464c0d04fdd49c41752a37c5
GSC Bravo <gscbravo@gmail.com>
2023-02-11 17:10:48 -0600

    styling

--- src/app.py
+++ src/app.py
@@ -22,11 +22,11 @@ MAX_COMMENTS = 1000
 # max chars in comment
 MAX_COMMENT_LENGTH = 2000
 # default name
-DEFAULT_NAME = "Guest"
+DEFAULT_NAME = 'Guest'
 # site name
-SITE_NAME = "Infinity Forums"
+SITE_NAME = 'Infinity Forums'
 # default board name
-DEFAULT_BOARD = "general"
+DEFAULT_BOARD = 'general'
 
 app = Flask(__name__)
 
@@ -34,54 +34,54 @@ app = Flask(__name__)
 comments = {}
 posts = 0
 
-@app.route("/")
+@app.route('/')
 def index():
-    return redirect(f"/b/{DEFAULT_BOARD}/")
+    return redirect(f'/b/{DEFAULT_BOARD}/')
 
-@app.route("/boards")
+@app.route('/boards')
 def list_boards():
-    return render_template("boards.html", all_boards=list(comments.keys()), site_name=SITE_NAME)
+    return render_template('boards.html', all_boards=list(comments.keys()), site_name=SITE_NAME)
 
-@app.route("/b/<board>/")
+@app.route('/b/<board>/')
 def load_board(board):
     req_board = board.lower().strip()
     # save space by just using empty array if no comments
     board_comments = comments[req_board] if req_board in comments else []
-    tag = request.args.get("tag", "")
-    return render_template("comments.html", comments=board_comments, tag=tag, default_name=DEFAULT_NAME, board=req_board, site_name=SITE_NAME)
+    tag = request.args.get('tag', '')
+    return render_template('comments.html', comments=board_comments, tag=tag, default_name=DEFAULT_NAME, board=req_board, site_name=SITE_NAME)
 
-@app.route("/go", methods=["GET", "POST"])
+@app.route('/go', methods=['GET', 'POST'])
 def go_to_board():
-    if request.method == "GET":
-        return render_template("error.html", error="Method not allowed")
-    redirect_board = request.form.get("board", "").lower().strip()
+    if request.method == 'GET':
+        return render_template('error.html', error='Method not allowed')
+    redirect_board = request.form.get('board', '').lower().strip()
     if not redirect_board:
-        return render_template("error.html", error="Board name must not be empty")
-    return redirect(f"/b/{redirect_board}/") if redirect_board else redirect("/")
+        return render_template('error.html', error='Board name must not be empty')
+    return redirect(f'/b/{redirect_board}/') if redirect_board else redirect('/')
 
-@app.route("/b/<board>/submit", methods=["GET", "POST"])
+@app.route('/b/<board>/submit', methods=['GET', 'POST'])
 def submit(board):
-    if request.method == "GET":
-        return render_template("error.html", error="Method not allowed")
+    if request.method == 'GET':
+        return render_template('error.html', error='Method not allowed')
     # get form args name, subject, text, replyto; set to empty string if not represent
     # only text is going to be actually required to post
-    name = request.form.get("name", "").strip()
-    subject = request.form.get("subject", "").strip()
-    text = request.form.get("text", "").strip()
-    replyto = request.form.get("replyto", "").strip()
+    name = request.form.get('name', '').strip()
+    subject = request.form.get('subject', '').strip()
+    text = request.form.get('text', '').strip()
+    replyto = request.form.get('replyto', '').strip()
 
     req_board = board.lower().strip()
 
     # if replyto is set and not a number, error
     if replyto and not replyto.isdigit():
-        return render_template("error.html", error="Comment ID is invalid")
+        return render_template('error.html', error='Comment ID is invalid')
 
     # if text is empty, error
     if not text:
-        return render_template("error.html", error="Text box must not be empty")
+        return render_template('error.html', error='Text box must not be empty')
 
     if len(text) > MAX_COMMENT_LENGTH:
-        return render_template("error.html", error=f"Text must be no more than {MAX_COMMENT_LENGTH} characters")
+        return render_template('error.html', error=f'Text must be no more than {MAX_COMMENT_LENGTH} characters')
 
     # if name is empty, set to default name
     if not name:
@@ -109,16 +109,16 @@ def submit(board):
 
     # insert comment and return to post sent
     comment_data = {
-        "name": name,
-        "subject": subject,
-        "text": text.split("\n"),
-        "date": datetime.utcnow().isoformat(" ", "seconds"),
-        "id": post_id,
-        "replyto": replyto,
-        "replies": []
+        'name': name,
+        'subject': subject,
+        'text': text.split('\n'),
+        'date': datetime.utcnow().isoformat(' ', 'seconds'),
+        'id': post_id,
+        'replyto': replyto,
+        'replies': []
     }
     current_board.insert(0, comment_data)
-    return redirect(f"/b/{req_board}/")
+    return redirect(f'/b/{req_board}/')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
